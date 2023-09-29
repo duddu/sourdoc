c9387c12b065ddb29d04a2479aa1c3282cfecdf2
Davide Doronzo <davide.doronzo@gmail.com>
2023-09-29 10:35:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Sourdoc 🧑‍🍳
+
+[Work in progress]
+
+Compute all the main variables involved in Sourdoc making process.
+
+Made with Flutter.
---
+++ android/app/src/main/kotlin/com/example/sourdoc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sourdoc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
