d2c6258f80f46ddc78b4c1e4d908e9dafa33b928
Davide Doronzo <davide.doronzo@gmail.com>
2023-10-14 17:21:20 +0100

    Move web deploy monitor to release job

--- cli/web_deploy_monitor.sh
+++ cli/web_deploy_monitor.sh
@@ -5,8 +5,10 @@ set -u
 : "${COMMIT:?Variable not set or empty}"
 
 VERSION_URL="https://sourdoc.duddu.dev/version.json"
-
 SECONDS=0
+
+echo "Follow the deploy pipeline at https://dashboard.render.com/static/srv-ckkutdqv7m0s73cts10g/events"
+
 until [ "$(curl -s $VERSION_URL | jq -r '.build_number')" == "$COMMIT" ] && echo "🚀 Web deploy completed"
 do
   if (( SECONDS > 300 ))
