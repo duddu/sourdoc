64118b91e6619d7573549021898d076ae5459996
Davide Doronzo <davide.doronzo@gmail.com>
2023-10-16 12:28:25 +0100

    Update release build number

--- cli/web_deploy_monitor.sh
+++ cli/web_deploy_monitor.sh
@@ -2,14 +2,14 @@
 
 set -e
 set -u
-: "${COMMIT:?Variable not set or empty}"
+: "${BUILD_NUMBER:?Variable not set or empty}"
 
 VERSION_URL="https://sourdoc.duddu.dev/version.json"
 SECONDS=0
 
 echo "Follow the deploy pipeline at https://dashboard.render.com/static/srv-ckkutdqv7m0s73cts10g/events"
 
-until [ "$(curl -s $VERSION_URL | jq -r '.build_number')" == "$COMMIT" ] && echo "🚀 Web deploy completed"
+until [ "$(curl -s $VERSION_URL | jq -r '.build_number')" == "$BUILD_NUMBER" ] && echo "🚀 Web deploy completed"
 do
   if (( SECONDS > 300 ))
   then
